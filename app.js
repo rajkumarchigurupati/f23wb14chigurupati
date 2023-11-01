4d644c3af560bc8701c21fb9fb657d640ec9c315
rajkumarchigurupati <S567114@nwmissouri.edu>
2023-11-01 12:03:58 -0500

    choose pug

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var DogRouter = require('./routes/Dog');
 var boardRouter = require('./routes/board')
-//var chooseRouter = require('./routes/choose')
+var chooseRouter = require('./routes/choose')
 
 var app = express();
 
@@ -26,7 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Dog',DogRouter);
 app.use('/board',boardRouter);
-//app.use('/choose',chooseRouter);
+app.use('/choose',chooseRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
