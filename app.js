e7c14a72c1cd38c9f79cc94aee409e460a50e453
rajkumarchigurupati <S567114@nwmissouri.edu>
2023-11-01 11:53:57 -0500

    committing part 8

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var DogRouter = require('./routes/Dog');
-//var boardRouter = require('./routes/board')
+var boardRouter = require('./routes/board')
 //var chooseRouter = require('./routes/choose')
 
 var app = express();
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Dog',DogRouter);
-//app.use('/board',boardRouter);
+app.use('/board',boardRouter);
 //app.use('/choose',chooseRouter);
 
 // catch 404 and forward to error handler
